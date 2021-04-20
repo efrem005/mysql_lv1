696ec70d2b826dcac8148874ee05ab8f47b568a1
efremov nikolay <efrem_nn@mail.ru>
2021-04-20 22:36:54 +0700

    Исправил опечатался CURDATE() вместо CURTIME()

--- lesson9/lesson9.sql
+++ lesson9/lesson9.sql
@@ -83,7 +83,7 @@ DELIMITER //
 CREATE PROCEDURE hello()
 BEGIN
     CASE
-        WHEN CURDATE() BETWEEN '06:00:00' AND '12:00:00' THEN
+        WHEN CURTIME() BETWEEN '06:00:00' AND '12:00:00' THEN
             SELECT 'Доброе утро';
         WHEN CURTIME() BETWEEN '12:00:00' AND '18:00:00' THEN
             SELECT 'Добрый день';
